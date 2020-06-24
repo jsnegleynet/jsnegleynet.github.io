597a9e8e94ddd61014b4be063b5e67ff976276ea
James Negley <james@jsnegley.com>
2020-06-24 16:38:28 -0500

    initial snapshot

---
+++ js/app.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+
+	jQuery('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+    	event.preventDefault(); 
+		event.stopPropagation(); 
+		jQuery(this).parent().addClass('open');
+		var menu = $(this).parent().find("ul");
+		var menupos = menu.offset();
+  
+	    if ((menupos.left + menu.width()) + 30 > $(window).width()) {
+    	    var newpos = - menu.width();      
+	    } else {
+    	    var newpos = $(this).parent().width();
+	    }
+    	menu.css({ left:newpos });
+	});
+
+});
